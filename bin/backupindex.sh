67c82c13dd21e2f4a7d0c11e01b8584242855d61
Woonsan Ko <woonsanko@users.noreply.github.com>
2018-01-11 14:37:19 -0500

    Update backupindex.sh

--- bin/backupindex.sh
+++ bin/backupindex.sh
@@ -15,23 +15,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-INDEX_DIR="/data"
-INDEX_SL_ZIP="index-export-latest.zip"
-INDEX_TS_ZIP="index-export-$(date +'%Y%m%d-%H%M%S').zip"
-INDEX_URL="http://server1.example.com:8080/cms/ws/indexexport"
-#INDEX_URL="http://localhost:8080/cms/ws/indexexport"
+# Local index backup directory path.
+LOCAL_INDEX_BACKUP_DIR="/data"
 
-mkdir -p INDEX_DIR
-curl --user admin:admin ${INDEX_URL} -o ${INDEX_DIR}/${INDEX_TS_ZIP}
+# Index export backup URLs as space separated string for optionally multiple URL(s).
+INDEX_URLS="http://server1.example.com:8080/cms/ws/indexexport"
+#INDEX_URLS="http://localhost:8080/cms/ws/indexexport"
 
-if [ ! $? -eq 0 ]; then
-  echo "Failed to download the latest index export zip file from '${INDEX_URL}'."
+# The symbolic link file name to the latest index backup zip file.
+LOCAL_INDEX_SL_ZIP="index-export-latest.zip"
+# Local index backup download file name with the current timestamp.
+LOCAL_INDEX_TS_ZIP="index-export-$(date +'%Y%m%d-%H%M%S').zip"
+
+mkdir -p LOCAL_INDEX_BACKUP_DIR
+
+# Loop each index export download URL and break the loop when successful.
+for INDEX_URL in $INDEX_URLS; do
+  curl --user admin:admin ${INDEX_URL} -o ${LOCAL_INDEX_BACKUP_DIR}/${LOCAL_INDEX_TS_ZIP}
+  if [ $? -eq 0 ]; then
+    break
+  fi
+done
+
+# Fail if it failed to download index export zip file.
+if [ ! -f "${LOCAL_INDEX_BACKUP_DIR}/${LOCAL_INDEX_TS_ZIP}" ]; then
+  echo "Failed to download index export zip file."
   exit 1
 fi
 
 # Remove the existing symbolic link if exists.
-if [ -f "${INDEX_DIR}/${INDEX_SL_ZIP}" ]; then
-  rm ${INDEX_DIR}/${INDEX_SL_ZIP}
+if [ -f "${LOCAL_INDEX_BACKUP_DIR}/${LOCAL_INDEX_SL_ZIP}" ]; then
+  rm ${LOCAL_INDEX_BACKUP_DIR}/${LOCAL_INDEX_SL_ZIP}
 fi
 
-ln -s ${INDEX_DIR}/${INDEX_TS_ZIP} ${INDEX_DIR}/${INDEX_SL_ZIP}
+ln -s ${LOCAL_INDEX_BACKUP_DIR}/${LOCAL_INDEX_TS_ZIP} ${LOCAL_INDEX_BACKUP_DIR}/${LOCAL_INDEX_SL_ZIP}
